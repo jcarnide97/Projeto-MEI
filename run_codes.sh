059c64b904027243df6ce88d7394fef7339113cf
jcarnide97 <joaonunes@student.dei.uc.pt>
2021-11-01 17:35:04 +0000

    faltam os testes de probabilidade 0.9

--- run_codes.sh
+++ run_codes.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
-gcc code1.c -o code1 -O3
-gcc code2.c -o code2 -O3
 count=10
 for run in {1..10}; do
 	val=`expr $run \* $count`
+	echo Output code1 com $val exames
 	for run2 in {1..30}; do
-		echo Output code1 com $val exames $run2:
-		./code1 31235 $1 testes/data_${val}exames10prob.in
+		./code1 31235 $1 testes/data_${val}exames90prob.in
 	done
+	echo Output code2 com $val exames
 	for run2 in {1..30}; do
-		echo Output code2 com $val exames $run2:
-		./code2 31235 $1 testes/data_${val}exames10prob.in
+		./code2 31235 $1 testes/data_${val}exames90prob.in
 	done
 done
