72c7d943a537b36c1c140da1597c8ffaea07a0c2
jcarnide97 <joaonunes@student.dei.uc.pt>
2021-12-19 19:28:14 +0000

    hypothesis 1 done

--- meta3/hipotese1.R
+++ meta3/hipotese1.R
@@ -36,11 +36,6 @@ anova_test <- function(file_name) {
   plot(aov.out)
 }
 
-box <- function(file_name) {
-  # read file and do a boxplot to show the time differences
-  csv <- read.table(file_name, header = TRUE, sep = ",")
-}
-
 samples = 100
 ttest <- function(file_name, sample_name) {
   csv <- read.table(file_name, header = TRUE, sep = ",")
@@ -48,9 +43,11 @@ ttest <- function(file_name, sample_name) {
   code1 <- vcodes[1:100]
   code2 <- vcodes[101:200]
   diff <- code1 - code2
+  shapiro.test(diff)
   qqnorm(diff, main = sample_name)
   qqline(diff)
-  print(t.test(code1, code2, paired = TRUE, conf.level = 0.95))
+  t.test(code1, code2, paired = TRUE, conf.level = 0.95)
+  print(wilcox.test(code1, code2, paired = TRUE))
 }
 
 ttest2 <- function(file_name) {
@@ -71,6 +68,35 @@ ttest2 <- function(file_name) {
   print(t)
 }
 
+valor_critico = 1956
+ranked <- function(file_name) {
+  csv <- read.table(file_name, header = TRUE, sep = ",")
+  vcodes <- unlist(csv, use.names = FALSE)
+  code1 <- vcodes[1:100]
+  code2 <- vcodes[101:200]
+  diff <- code1 - code2
+  sinal <- sign(diff)
+  frank <- rank(diff)
+  sumPos <- 0
+  sumNeg <- 0
+  for (i in 1:100) {
+    if (sinal[i] < 0) {
+      sumNeg = sumNeg + frank[i]
+    } else if (sinal[i] > 0) {
+      sumPos = sumPos + frank[i]
+    }
+  }
+  print(sumNeg)
+  print(sumPos)
+}
+
+ranked("testes/hipotese1/h1_30ex30prob_input1.csv")
+ranked("testes/hipotese1/h1_30ex30prob_input2.csv")
+ranked("testes/hipotese1/h1_30ex30prob_input3.csv")
+ranked("testes/hipotese1/h1_40ex10prob_input1.csv")
+ranked("testes/hipotese1/h1_40ex10prob_input2.csv")
+ranked("testes/hipotese1/h1_40ex10prob_input3.csv")
+
 # par(mfrow = c(1,3))
 # ttest("testes/hipotese1/h1_30ex30prob_input1.csv", "Sample 1 - 30 Exames/Prob 0.3")
 # ttest("testes/hipotese1/h1_30ex30prob_input2.csv", "Sample 2 - 30 Exames/Prob 0.3")
@@ -78,3 +104,4 @@ ttest2 <- function(file_name) {
 # ttest("testes/hipotese1/h1_40ex10prob_input1.csv", "Sample 1 - 40 Exames/Prob 0.1")
 # ttest("testes/hipotese1/h1_40ex10prob_input2.csv", "Sample 2 - 40 Exames/Prob 0.1")
 # ttest("testes/hipotese1/h1_40ex10prob_input3.csv", "Sample 3 - 40 Exames/Prob 0.1")
+#valor critico para um sample de 100 = 1956
--- meta3/hipotese1_box.R
+++ meta3/hipotese1_box.R
@@ -1,7 +1,12 @@
-boxcode <- function(file_name) {
+boxcode <- function(file_name, title) {
   csv <- read.table(file_name, header = TRUE, sep = ",")
-  janela <- c(0,10)
-  boxplot(Time ~ Code, data = csv, ylim=janela, main = "Boxplot Samples 1 - 30 Exames/Prob 0.3")
+  if (file_name == "testes/hipotese1/h1_30ex30prob_input1_box.csv") {
+    janela <- c(0,10)
+  } else if (file_name == "testes/hipotese1/h1_40ex10prob_input1_box.csv") {
+    janela <- c(0,2)
+  }
+  boxplot(Time ~ Code, data = csv, ylim=janela, main = title)
 }
-
-boxcode("testes/hipotese1/h1_30ex30prob_input1_box.csv")
+par(mfrow = c(1,2))
+boxcode("testes/hipotese1/h1_30ex30prob_input1_box.csv", "Boxplot Samples 1 - 30 Exames/Prob 0.3")
+boxcode("testes/hipotese1/h1_40ex10prob_input1_box.csv", "Boxplot Samples 1 - 40 Exames/Prob 0.1")
